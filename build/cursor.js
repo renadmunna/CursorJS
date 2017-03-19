df0500f89c9211a973e82a5108cc3ce638c93b0d
Md. Mohsin Ali <alimohsin.pro@gmail.com>
2017-03-19 11:33:53 +0600

    Full add

---
+++ build/cursor.js
@@ -0,0 +1,31 @@
+var cursor=function(cursorRef){
+  if(cursorRef.type=="img"){cursorRef.url=cursorRef.filePath;}
+  else if(cursorRef.type=="svg"){
+    if(cursorRef.hasOwnProperty("svgElement")){
+      cursorRef.url="data:image/svg+xml;base64,"+btoa((new XMLSerializer).serializeToString(cursorRef.svgElement));
+    }
+	else if(cursorRef.hasOwnProperty("svgString")){
+	  var tempDomElement=document.createElement('div');
+      tempDomElement.innerHTML=cursorRef.svgString;
+	  console.log(tempDomElement.getElementsByTagName("svg")[0]);
+	  cursorRef.url="data:image/svg+xml;base64,"+btoa((new XMLSerializer).serializeToString(tempDomElement.firstChild));
+	}
+    else if(cursorRef.hasOwnProperty("filePath")){
+	  var xhr=new XMLHttpRequest();
+      xhr.open("GET",cursorRef.filePath);
+      xhr.send();
+      xhr.onreadystatechange=function() {
+        if (xhr.readyState===XMLHttpRequest.DONE && xhr.status===200) {
+		  var tempDomElement=document.createElement('div');
+          tempDomElement.innerHTML=xhr.responseText;
+		  console.log(tempDomElement.getElementsByTagName("svg")[0]);
+		  cursorRef.url="data:image/svg+xml;base64,"+btoa((new XMLSerializer).serializeToString(tempDomElement.firstChild));
+		  cursorRef.domElement.style["cursor"]="url("+cursorRef.url+"),auto";
+        }
+      };
+	}
+  }
+  if(cursorRef.url!=undefined && cursorRef.url!=null && cursorRef.url!=""){
+    cursorRef.domElement.style["cursor"]="url("+cursorRef.url+"),auto";
+  }
+};
